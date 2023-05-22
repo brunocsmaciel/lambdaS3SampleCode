ce9f1d07b0cc71a889f30f6fe8422d7efead1bb1
brunocsmaciel <branis.cesar18@gmail.com>
2023-05-22 19:38:14 -0300

    commit inicial

---
+++ src/main/java/br/com/parcaseparceiros/lambdaeventobuckets/Handler.java
@@ -0,0 +1,46 @@
+package br.com.parcaseparceiros.lambdaeventobuckets;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class Handler implements RequestHandler<String, String> {
+
+    private final String nomeBucket = "bucket-parcas-parceiros";
+
+
+    @Override
+    public String handleRequest(String textoEntrada, Context context) {
+
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
+
+        try {
+            var conteudo = textoEntrada;
+
+            InputStream inputStream = new ByteArrayInputStream(conteudo.getBytes(StandardCharsets.UTF_8));
+
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(conteudo.length());
+
+            s3Client.putObject(new PutObjectRequest(nomeBucket, "example.txt", inputStream, metadata));
+
+            return "Put executado com sucesso!";
+
+        } catch (Exception e) {
+            return "Erro ao postar no bucket" + e.getMessage();
+        }
+
+    }
+
+
+
+
+}
